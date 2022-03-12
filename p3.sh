0c0f4e11db182f90ac6609017c707036f39f65e6
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 23:11:52 +0530

    Update p3.sh

--- p3.sh
+++ p3.sh
@@ -1,3 +1,5 @@
+# Extend the Prime Factorization Program to store all the Prime Factors of a number n into an array and finally display the output.
+
 echo "Enter any number you want to prime factorize : "
 read n
 if(($n==0 || $n==1))
